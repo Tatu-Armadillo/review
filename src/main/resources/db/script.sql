aa2e02ab8dacd364bcccb8d56ce2333919b7d658
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2024-08-20 12:59:15 -0400

    feat: create script.sql db

---
+++ src/main/resources/db/script.sql
@@ -0,0 +1,107 @@
+do $$ 
+begin 
+    if not exists (
+        select
+            1
+        from
+            information_schema.schemata
+        where
+            schema_name = 'restaurant_review'
+    ) 
+    then 
+        drop schema if exists restaurant_review cascade; -- REMOVE;
+
+        create schema restaurant_review;
+
+        create table permission (
+            id_permission bigserial primary key,
+            description varchar(255) unique
+        );
+
+        create table users(
+            id_user bigserial primary key,
+            cpf varchar(11) unique not null,
+            phone varchar(13) unique not null,
+            user_name varchar(255) unique not null,
+            full_name varchar(255) not null,
+            password varchar(255) not null,
+            account_non_expired boolean not null,
+            account_non_locked boolean not null,
+            credentials_non_expired boolean not null,
+            enabled boolean not null
+        );
+
+        create table user_permission(
+            id_user bigint,
+            id_permission bigint,
+            primary key (id_user, id_permission)
+        );
+
+        create table tables(
+            id_table bigserial primary key,
+            capacity integer not null,
+            is_available boolean,
+            restaurant bigint not null
+        );
+
+        create table bookings(
+            id_booking bigserial primary key,
+            quantity_people integer not null,
+            reserved_date timestamp not null,
+            users bigint not null
+        );
+
+        create table tables_bookings(
+            id_tables_bookings bigserial primary key,
+            booking bigint not null,
+            tables bigint not null
+        );
+
+        create table reviews(
+            id_review bigserial primary key,
+            create_date timestamp not null,
+            grade integer not null,
+            comment text not null,
+            users bigint not null
+        );
+
+        create table restaurants (
+            id_restaurant bigserial primary key,
+            name varchar(255) not null,
+            phone varchar(13) not null,
+            food_type varchar(255) not null,
+            open_hour time not null,
+            close_hour time not null,
+            is_always_open boolean not null,
+            total_capacity integer not null,
+            address bigint
+        );
+
+        create table address(
+            id_address bigserial primary key,
+            cep varchar(8) not null,
+            public_place varchar(255),
+            complement varchar(255),
+            neighborhood varchar(255),
+            city varchar(255),
+            uf_state varchar(2),
+            latitude_Y numeric(9, 6),
+            longitude_X numeric(9, 6)
+        );
+
+        alter table user_permission add constraint fk_user_permission_users foreign key (id_user) references users (id_user);
+        alter table user_permission add constraint fk_user_permission_permission foreign key (id_permission) references permission (id_permission);
+        alter table tables add constraint fk_table_restaurant foreign key (restaurant) references restaurants (id_restaurant);
+        alter table bookings add constraint fk_booking_users foreign key (users) references users (id_user);
+        alter table tables_bookings add constraint fk_tables_bookings_table foreign key (tables) references tables (id_table);
+        alter table tables_bookings add constraint fk_tables_bookings_booking foreign key (booking) references bookings (id_booking);
+        alter table reviews add constraint fk_reviews_users foreign key (users) references users (id_user);
+        alter table restaurants add constraint fk_restaurants_address foreign key (address) references address (id_address);
+
+    --   password 123456
+    --   insert into challenge.users (user_name,full_name,"password",account_non_expired,account_non_locked,credentials_non_expired,enabled) VALUES ('adm','adm','$2a$10$PqsrFKSSRev9lL0BMAE.IOvDB4r6plBA7c45UDzz4v0Wu1Es9XMs.',true,true,true,true);
+    --   insert into challenge.permission (description) values ('ADMINISTRATOR');
+    --   insert into challenge.user_permission (id_user,id_permission) VALUES (1,1);
+    
+    end if;
+end $$;
