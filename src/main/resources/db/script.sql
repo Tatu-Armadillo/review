24522d0f2d611b3ed6b83ffcd9c72bea5964806d
Felipe Alves de Oliviera <felioliveira@mpmt.mp.br>
2024-08-21 13:27:58 -0400

    feat: create database with java application

--- src/main/resources/db/script.sql
+++ src/main/resources/db/script.sql
@@ -1,22 +1,25 @@
 do $$ 
 begin 
     if not exists (
-        select
-            1
-        from
-            information_schema.schemata
-        where
-            schema_name = 'restaurant_review'
-    ) 
-    then 
-        drop schema if exists restaurant_review cascade; -- REMOVE;
+        select 1
+        from information_schema.schemata 
+        where schema_name = 'challenge'
+    ) then 
+        drop schema if exists challenge cascade; -- REMOVE;
 
-        create schema restaurant_review;
+        create schema challenge;
 
-        create table permission (
-            id_permission bigserial primary key,
-            description varchar(255) unique
-        );
+        -- TABELA REFERENTE AO JWT
+        -- create table permission (
+        --     id_permission bigserial primary key,
+        --     description varchar(255) unique
+        -- );
+
+        -- create table user_permission(
+        --     id_user bigint,
+        --     id_permission bigint,
+        --     primary key (id_user, id_permission)
+        -- );
 
         create table users(
             id_user bigserial primary key,
@@ -24,23 +27,18 @@ begin
             phone varchar(13) unique not null,
             user_name varchar(255) unique not null,
             full_name varchar(255) not null,
-            password varchar(255) not null,
-            account_non_expired boolean not null,
-            account_non_locked boolean not null,
-            credentials_non_expired boolean not null,
-            enabled boolean not null
-        );
-
-        create table user_permission(
-            id_user bigint,
-            id_permission bigint,
-            primary key (id_user, id_permission)
+            password varchar(255) not null
+            -- COLUNAS REFERENTE AO JWT
+            -- account_non_expired boolean not null,
+            -- account_non_locked boolean not null,
+            -- credentials_non_expired boolean not null,
+            -- enabled boolean not null
         );
 
         create table tables(
             id_table bigserial primary key,
             capacity integer not null,
-            is_available boolean,
+            available boolean not null,
             restaurant bigint not null
         );
 
@@ -52,9 +50,9 @@ begin
         );
 
         create table tables_bookings(
-            id_tables_bookings bigserial primary key,
-            booking bigint not null,
-            tables bigint not null
+            id_booking bigint not null,
+            id_table bigint not null,
+            primary key (id_booking, id_table)
         );
 
         create table reviews(
@@ -62,7 +60,8 @@ begin
             create_date timestamp not null,
             grade integer not null,
             comment text not null,
-            users bigint not null
+            users bigint not null,
+            restaurant bigint not null
         );
 
         create table restaurants (
@@ -72,8 +71,9 @@ begin
             food_type varchar(255) not null,
             open_hour time not null,
             close_hour time not null,
-            is_always_open boolean not null,
+            always_open boolean not null,
             total_capacity integer not null,
+            total_grade integer not null,
             address bigint
         );
 
@@ -88,16 +88,18 @@ begin
             latitude_Y numeric(9, 6),
             longitude_X numeric(9, 6)
         );
-
-        alter table user_permission add constraint fk_user_permission_users foreign key (id_user) references users (id_user);
-        alter table user_permission add constraint fk_user_permission_permission foreign key (id_permission) references permission (id_permission);
+       
         alter table tables add constraint fk_table_restaurant foreign key (restaurant) references restaurants (id_restaurant);
         alter table bookings add constraint fk_booking_users foreign key (users) references users (id_user);
-        alter table tables_bookings add constraint fk_tables_bookings_table foreign key (tables) references tables (id_table);
-        alter table tables_bookings add constraint fk_tables_bookings_booking foreign key (booking) references bookings (id_booking);
-        alter table reviews add constraint fk_reviews_users foreign key (users) references users (id_user);
+        alter table tables_bookings add constraint fk_tables_bookings_table foreign key (id_table) references tables (id_table);
+        alter table tables_bookings add constraint fk_tables_bookings_booking foreign key (id_booking) references bookings (id_booking);
+        alter table reviews add constraint fk_review_user foreign key (users) references users (id_user);
+        alter table reviews add constraint fk_review_restaurant foreign key (restaurant) references restaurants (id_restaurant);
         alter table restaurants add constraint fk_restaurants_address foreign key (address) references address (id_address);
 
+    --   TABELA REFERENTE AO JWT
+    --   alter table user_permission add constraint fk_user_permission_permission foreign key (id_permission) references permission (id_permission);
+    --   alter table user_permission add constraint fk_user_permission_users foreign key (id_user) references users (id_user);
     --   password 123456
     --   insert into challenge.users (user_name,full_name,"password",account_non_expired,account_non_locked,credentials_non_expired,enabled) VALUES ('adm','adm','$2a$10$PqsrFKSSRev9lL0BMAE.IOvDB4r6plBA7c45UDzz4v0Wu1Es9XMs.',true,true,true,true);
     --   insert into challenge.permission (description) values ('ADMINISTRATOR');
