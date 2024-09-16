1e116aad2d2db0e11f24b1f06ef46bb81d55414d
CiranoB <ciranobelardony@gmail.com>
2024-09-15 22:03:31 -0300

    Adding TODO and removing unused methods

--- src/main/java/com/fiap/restaurant/review/application/records/restaurant/RestaurantRecord.java
+++ src/main/java/com/fiap/restaurant/review/application/records/restaurant/RestaurantRecord.java
@@ -2,8 +2,6 @@ package com.fiap.restaurant.review.application.records.restaurant;
 
 import java.time.LocalTime;
 
-import com.fiap.restaurant.review.infra.models.RestaurantModel;
-
 public record RestaurantRecord(
         String name,
         String cnpj,
@@ -14,37 +12,7 @@ public record RestaurantRecord(
         Boolean alwaysOpen,
         Integer totalCapacity,
         AddressRecord address) 
-        
-        // String name, String cnpj, String phone, String foodType, LocalTime openHour,
-        //     LocalTime closeHour, Boolean alwaysOpen, Integer totalCapacity, Integer totalGrade, AddressEntity address
-        
         {
 
-    public static RestaurantModel toEntity(final RestaurantRecord record) {
-        final var entity = new RestaurantModel();
-        entity.setName(record.name);
-        entity.setPhone(record.phone);
-        entity.setFoodType(record.foodType);
-        entity.setCnpj(record.cnpj);
-        entity.setOpenHour(record.openHour);
-        entity.setCloseHour(record.closeHour);
-        entity.setAlwaysOpen(record.alwaysOpen);
-        entity.setTotalCapacity(record.totalCapacity);
-        entity.setAddress(AddressRecord.toEntity(record.address));
-        return entity;
-    }
-
-    public static RestaurantRecord toRecord(final RestaurantModel entity) {
-        return new RestaurantRecord(
-                entity.getName(),
-                entity.getPhone(),
-                entity.getFoodType(),
-                entity.getCnpj(),
-                entity.getOpenHour(),
-                entity.getCloseHour(),
-                entity.getAlwaysOpen(),
-                entity.getTotalCapacity(),
-                AddressRecord.toRecord(entity.getAddress()));
-    }
 
 }
--- src/main/java/com/fiap/restaurant/review/domain/usecases/restaurant/SaveRestaurantUseCase.java
+++ src/main/java/com/fiap/restaurant/review/domain/usecases/restaurant/SaveRestaurantUseCase.java
@@ -17,6 +17,7 @@ public class SaveRestaurantUseCase {
     private final SaveRestaurantInterface saveRestaurantRepository;
     private OutputInterface saveRestaurantOutput;
 
+    // TODO: Add presenter layer to remove ID from the response
     public void execute(SaveRestaurantInput saveRestaurantInput) {
         RestaurantEntity restaurantEntity = new RestaurantEntity(
             saveRestaurantInput.name(),
