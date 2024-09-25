d83998f65c758dba87df3533c697fe5e36e81067
Tatu-Armadillo <prohazor.fa@gmail.com>
2024-09-25 00:18:19 -0300

    adicionando parametro em teste

--- src/test/java/com/fiap/restaurant/review/application/controllers/mock/BookingModelTestData.java
+++ src/test/java/com/fiap/restaurant/review/application/controllers/mock/BookingModelTestData.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public class BookingModelTestData {
 
     public static BookingModel createBooking() {
-        return new BookingModel(1L, 4, LocalDateTime.now(),
+        return new BookingModel(1L, 4, LocalDateTime.now(), false,
                 UserModelTestData.createUser(), TableModelTestData.createTable());
     }
 }
