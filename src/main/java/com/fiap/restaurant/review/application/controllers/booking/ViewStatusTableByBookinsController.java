4b3258d6804fc647cc33d63d12f3b7f40bb010d4
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2024-09-26 16:57:54 -0400

    fix: rename

--- src/main/java/com/fiap/restaurant/review/application/controllers/booking/ViewStatusTableByBookinsController.java
+++ src/main/java/com/fiap/restaurant/review/application/controllers/booking/ViewStatusTableByBookinsController.java
@@ -1,27 +1,28 @@
 package com.fiap.restaurant.review.application.controllers.booking;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import com.fiap.restaurant.review.domain.generic.output.OutputInterface;
 import com.fiap.restaurant.review.domain.input.booking.BookingWithTableStatusFilter;
 import com.fiap.restaurant.review.domain.usecases.booking.GetAllTablesWithBookings;
 import com.fiap.restaurant.review.infra.adapter.repository.booking.GetAllTablesWithBookingsRepository;
 import com.fiap.restaurant.review.infra.repositories.BookingRepositoy;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/booking/status")
 @Tag(name = "Bookings", description = "Endpoints for views tables availabes by bookings")
-public class ViewStatusTableByBookins {
+public class ViewStatusTableByBookinsController {
 
     private final BookingRepositoy bookingRepositoy;
 
